8984f46eabe0c862c8ed75147fb9644f06fbbb84
unknown <zachariah11jerobert@gmail.com>
2022-01-03 09:30:51 +0530

    basic flow of query & mutation

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-  res.send("hello World!");
-});
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+      type RootQuery {
+        events: [String!]!
+      }
+
+      type RootMutation {
+        createEvent(name: String): String
+      }
+
+      schema {
+        query:RootQuery
+        mutation:RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return ["Romantic Cooking", "Sailing", "All-Night-Coding"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
 
 app.listen(3000);
