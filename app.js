ec00f3b29c78efb005836e0ec1350a976bf8e740
unknown <zachariah11jerobert@gmail.com>
2022-01-04 05:55:39 +0530

    done mongodb connections and logic written

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHTTP = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const Event = require("./models/event");
+const User = require("./models/user");
 
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -21,11 +24,22 @@ app.use(
         date: String!
       }
 
+      type User {
+        _id: ID!
+        email: String!
+        password: String
+      }
+
       input EventInput {
         title: String!
         description: String!
         price: Float!
         date: String!
+      }
+
+      input UserInput {
+        email: String!
+        password: String!
       }
 
       type RootQuery {
@@ -34,6 +48,7 @@ app.use(
 
       type RootMutation {
         createEvent(eventInput: EventInput): Event
+        createUser(userInput: UserInput): User
       }
 
       schema {
@@ -43,22 +58,79 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+          .then((events) => {
+            return events.map((event) => {
+              return { ...event._doc, _id: event.id };
+            });
+          })
+          .catch((err) => {
+            throw err;
+          });
       },
       createEvent: (args) => {
-        const event = {
-          _id: Math.random().toString(),
+        const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: args.eventInput.date,
-        };
-        events.push(event);
-        return event;
+          date: new Date(args.eventInput.date),
+          creator: "61d38e0459ae51428cfa6dd8",
+        });
+        let createdEvent;
+        return event
+          .save()
+          .then((result) => {
+            createdEvent = { ...result._doc, _id: event._doc._id.toString() };
+            return User.findById("61d38e0459ae51428cfa6dd8");
+          })
+          .then((user) => {
+            if (!user) {
+              throw new Error("User not found.");
+            }
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then((result) => {
+            return createdEvent;
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
+      },
+      createUser: (args) => {
+        return User.findOne({ email: args.userInput.email })
+          .then((user) => {
+            if (user) {
+              throw new Error("User exists already.");
+            }
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+
+          .then((hashedPasword) => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPasword,
+            });
+            return user.save();
+          })
+          .then((result) => {
+            return { ...result._doc, password: null, id: result.id };
+          })
+          .catch((err) => {
+            throw err;
+          });
       },
     },
     graphiql: true,
   })
 );
 
-app.listen(3000);
+mongoose
+  .connect("mongodb://localhost:27017/graphql-academind")
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
